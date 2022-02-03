48aa6e91ff240c9d231b7f79004b4d01618a1d4c
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-02-03 15:02:05 +0300

    create PUT and DELETE item controller functions

--- src/controllers/item.js
+++ src/controllers/item.js
@@ -18,13 +18,41 @@ module.exports = {
   getFilteredItems: (req, res) => {
     res.send('Filtered items');
   },
-  updateItem: (req, res) => {
-    res.send('Item with the specified ID has been updated');
+  updateItem: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const updatedItem = await ItemModel.findByIdAndUpdate(
+        id,
+        { $set: req.body },
+        { new: true }
+      );
+      if (!updatedItem) {
+        res
+          .status(422)
+          .json({ message: 'The item with the specified ID was not found.' });
+      } else {
+        res.json(updatedItem);
+      }
+    } catch (err) {
+      res.status(422).json({ message: err.message });
+    }
   },
   addItem: (req, res) => {
     res.send('New item has been added');
   },
-  deleteItem: (req, res) => {
-    res.send('Item with the specified ID has been deleted');
+  deleteItem: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const item = await ItemModel.findByIdAndDelete(id);
+      if (!item) {
+        res
+          .status(422)
+          .json({ message: 'The item with the specified ID was not found.' });
+      } else {
+        res.status(204).json({ message: 'The item is successfully deleted.' });
+      }
+    } catch (err) {
+      res.status(422).json({ message: err.message });
+    }
   },
 };
