23816cc395b58183d55757a9f71691de8d5802bf
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-02-04 15:44:57 +0300

    add user controller functions

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,20 +1,66 @@
+const UserModel = require('../models/user');
+
 module.exports = {
-  getAllUsers: (req, res) => {
-    res.send('All users');
+  getAllUsers: async (req, res) => {
+    try {
+      const users = await UserModel.find();
+      res.json(users);
+    } catch (err) {
+      res.status(422).json({ message: err.message });
+    }
   },
-  getSingleUser: (req, res) => {
-    res.send('Single user');
+  getSingleUser: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const user = await UserModel.findById(id);
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(422).json({ message: err.message });
+    }
   },
-  updateUser: (req, res) => {
-    res.send('User with the specified ID has been updated');
+  updateUser: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const updatedUser = await UserModel.findByIdAndUpdate(
+        id,
+        {
+          $set: req.body,
+        },
+        {
+          new: true,
+        }
+      );
+
+      if (!updatedUser) {
+        res
+          .status(422)
+          .json({ message: "The user with the specified id wasn't found" });
+      } else {
+        res.json(updatedUser);
+      }
+    } catch (err) {
+      res.status(422).json({ message: err.message });
+    }
   },
-  addUser: (req, res) => {
-    res.send('New user has been added');
+  addUser: async (req, res) => {
+    const user = req.body;
+    try {
+      const newUser = await UserModel.create(user);
+      res.status(201).json(newUser);
+    } catch (err) {
+      res.status(422).json({ message: err.message });
+    }
   },
-  deleteUser: (req, res) => {
-    res.send('User with the specified ID has been deleted');
+  deleteUser: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const user = await UserModel.findByIdAndRemove(id);
+      res.status(204).json(user);
+    } catch (err) {
+      res.status(422).json({ message: err.message });
+    }
   },
-  getAllDonators: (req, res) => {
+  getAllDonators: async (req, res) => {
     res.send('All donators');
   },
 };
