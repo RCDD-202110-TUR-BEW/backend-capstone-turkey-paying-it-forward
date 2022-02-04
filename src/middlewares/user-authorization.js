b1c1e2f7d9106f3717a12c2a06772efad37916b7
mahmoud <mahmoudalkasem1997@gmail.com>
2022-02-04 23:45:38 +0300

    add user authorization middleware

---
+++ src/middlewares/user-authorization.js
@@ -0,0 +1,10 @@
+// this middlewawre is called after the the isAuth middleware
+// and checks if the user is authorized to access the route
+module.exports = function (req, res, next) {
+  // eslint-disable-next-line no-underscore-dangle
+  if (req.user._id === req.params.id) {
+    next();
+  } else {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+};
