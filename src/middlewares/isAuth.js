5afd4c7d057b1c03e0701ce384cec85365877db7
mahmoud <mahmoudalkasem1997@gmail.com>
2022-02-04 23:40:03 +0300

    fix user retrieval from the token

--- src/middlewares/isAuth.js
+++ src/middlewares/isAuth.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken');
 module.exports = async (req, res, next) => {
   const { token } = req.cookies;
   try {
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const { user } = jwt.verify(token, process.env.JWT_SECRET);
     // if verified pass the user to the next function with the req object
-    req.user = decodedToken;
+    req.user = user;
     return next();
   } catch (error) {
     // delete the token when it is invalid
