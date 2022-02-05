b57b3f336a9d20cb8fa29fc6113f6ddaa22f6b30
mahmoud <mahmoudalkasem1997@gmail.com>
2022-02-05 20:47:08 +0300

    edit if statement in item-authorization file

--- src/middlewares/item-authorization.js
+++ src/middlewares/item-authorization.js
@@ -4,7 +4,7 @@ const Item = require('../models/item');
 module.exports = async (req, res, next) => {
   const itemId = req.params.id;
   const item = await Item.findById(itemId);
-  if (item && item.owner._id.toString() === req.user._id) {
+  if (item && item.owner.toString() === req.user._id) {
     next();
   } else {
     res.status(401).json({ message: 'Unauthorized' });
