40172ac2272a40fede5ef4e7cbdbcf5f33481b01
mahmoud <mahmoudalkasem1997@gmail.com>
2022-02-08 19:33:34 +0300

    add try Catch block to user-auth middleware

--- src/middlewares/user-authorization.js
+++ src/middlewares/user-authorization.js
@@ -1,10 +1,14 @@
 // this middleware is called after the the isAuth middleware
 // and checks if the user is authorized to access the route
 module.exports = function (req, res, next) {
-  // eslint-disable-next-line no-underscore-dangle
-  if (req.user._id === req.params.id) {
-    next();
-  } else {
+  try {
+    // eslint-disable-next-line no-underscore-dangle
+    if (req.user._id === req.params.id) {
+      next();
+    } else {
+      res.status(401).json({ message: 'Unauthorized' });
+    }
+  } catch (error) {
     res.status(401).json({ message: 'Unauthorized' });
   }
 };
