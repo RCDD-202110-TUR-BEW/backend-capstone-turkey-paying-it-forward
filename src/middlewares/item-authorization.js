f9a7bf0ad828a2c1cfd6bb47362ff9ae16182f6a
mahmoud <mahmoudalkasem1997@gmail.com>
2022-02-08 19:48:42 +0300

    add try Catch block to item-auth middleware

--- src/middlewares/item-authorization.js
+++ src/middlewares/item-authorization.js
@@ -1,12 +1,16 @@
-/* eslint-disable no-underscore-dangle */
 const Item = require('../models/item');
 
 module.exports = async (req, res, next) => {
-  const itemId = req.params.id;
-  const item = await Item.findById(itemId);
-  if (item && item.owner.toString() === req.user._id) {
-    next();
-  } else {
+  try {
+    const itemId = req.params.id;
+    const item = await Item.findById(itemId);
+    /* eslint-disable no-underscore-dangle */
+    if (item && item.owner.toString() === req.user._id) {
+      next();
+    } else {
+      res.status(401).json({ message: 'Unauthorized' });
+    }
+  } catch (error) {
     res.status(401).json({ message: 'Unauthorized' });
   }
 };
