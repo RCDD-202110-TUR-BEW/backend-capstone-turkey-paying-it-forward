97740b8a8df0f7b6a80ea4d7d9dd622d74d102da
mahmoud <mahmoudalkasem1997@gmail.com>
2022-02-13 17:17:40 +0300

    add sendEmail function

---
+++ src/services/mail.js
@@ -0,0 +1,26 @@
+/* this function is used to send emails to users
+    it takes in an object with the following properties:
+    from: the sender's email address
+    to: the recipient's email address
+    subject: the subject of the email
+    html: the html body of the email
+*/
+const nodemailer = require('nodemailer');
+
+module.exports = {
+  sendEmail: async (emailOptions) => {
+    try {
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.office365.com',
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+      const res = await transporter.sendMail(emailOptions);
+      return res;
+    } catch (error) {
+      return error.message;
+    }
+  },
+};
