f13c6afe394a2fd441fe51f164308881d4bf176f
mahmoud <mahmoudalkasem1997@gmail.com>
2022-02-20 16:03:29 +0300

    update cron test file with logger

--- src/services/__tests__/cron.test.js
+++ src/services/__tests__/cron.test.js
@@ -6,6 +6,7 @@ const User = require('../../models/user');
 const Item = require('../../models/item');
 const { checkServerStatusJob, newsLetterJob } = require('../cron');
 const mail = require('../mail');
+const logger = require('../logger');
 
 jest.mock('../../db/connection', () => ({
   connectToMongo: jest.fn(),
@@ -17,7 +18,8 @@ const spyOnFindItem = jest.spyOn(Item, 'find');
 const spyOnFindUser = jest.spyOn(User, 'find');
 const spyOnSchedule = jest.spyOn(cron, 'schedule');
 const spyOnAxiosGet = jest.spyOn(axios, 'get');
-const spyOnConsoleLog = jest.spyOn(console, 'log');
+const spyOnLoggerInfo = jest.spyOn(logger, 'info');
+const spyOnLoggerError = jest.spyOn(logger, 'error');
 
 afterAll(() => {
   jest.clearAllMocks();
@@ -48,13 +50,13 @@ describe('cron jobs service', () => {
       expect(spyOnAxiosGet).toHaveBeenCalledWith(
         'http://localhost:3000/status'
       );
-      expect(spyOnConsoleLog).toHaveBeenCalled();
-      expect(spyOnConsoleLog).toBeCalledTimes(1);
-      expect(spyOnConsoleLog).toBeCalledWith('Server is up');
+      expect(spyOnLoggerInfo).toHaveBeenCalled();
+      expect(spyOnLoggerInfo).toBeCalledTimes(1);
+      expect(spyOnLoggerInfo).toBeCalledWith('Server is up');
 
       spyOnAxiosGet.mockClear();
       spyOnSchedule.mockClear();
-      spyOnConsoleLog.mockClear();
+      spyOnLoggerInfo.mockClear();
     });
     test('Should log server is down when server is not working', async () => {
       spyOnAxiosGet.mockImplementation(() => {
@@ -81,12 +83,11 @@ describe('cron jobs service', () => {
       expect(spyOnAxiosGet).toHaveBeenCalledWith(
         'http://localhost:3000/status'
       );
-      expect(spyOnConsoleLog).toHaveBeenCalled();
-      expect(spyOnConsoleLog).toBeCalledTimes(1);
-      expect(spyOnConsoleLog).toBeCalledWith('Server is down');
+      expect(spyOnLoggerError).toHaveBeenCalled();
+      expect(spyOnLoggerError).toBeCalledTimes(1);
+      expect(spyOnLoggerError).toBeCalledWith('Server is down');
 
       spyOnAxiosGet.mockRestore();
-      spyOnConsoleLog.mockRestore();
       spyOnSchedule.mockClear();
     });
   });
@@ -179,6 +180,8 @@ describe('cron jobs service', () => {
       expect(spyOnFindUser).toHaveBeenCalledTimes(1);
       expect(spyOnFindUser).toHaveBeenCalledWith();
       expect(mail.sendEmail).not.toHaveBeenCalled();
+      expect(spyOnLoggerError).toHaveBeenCalledTimes(2);
+      expect(spyOnLoggerError).toHaveBeenCalledWith('email not sent');
       expect(connection.closeDatabase).toHaveBeenCalled();
       expect(connection.closeDatabase).toHaveBeenCalledTimes(2);
 
@@ -223,6 +226,8 @@ describe('cron jobs service', () => {
       expect(spyOnFindUser).toHaveBeenCalledTimes(1);
       expect(spyOnFindUser).toHaveBeenCalledWith();
       expect(mail.sendEmail).not.toHaveBeenCalled();
+      expect(spyOnLoggerError).toHaveBeenCalledTimes(3);
+      expect(spyOnLoggerError).toHaveBeenCalledWith('email not sent');
       expect(connection.closeDatabase).toHaveBeenCalled();
       expect(connection.closeDatabase).toHaveBeenCalledTimes(3);
     });
