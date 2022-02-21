1fd614a9456ffb572c415c84717431c78f539869
Ammar Abdulbaki <ammarabdolbaqi4@gmail.com>
2022-02-22 00:03:16 +0300

    add swagger configuration to app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,13 @@
 const express = require('express');
-
 require('dotenv').config();
-
 const cookieParser = require('cookie-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const { connectToMongo } = require('./db/connection');
-
 const authRoutes = require('./routers/auth');
-
 const itemRoutes = require('./routers/item');
-
 const userRoutes = require('./routers/user');
-
 const globalRoutes = require('./routers/global');
-
 const logger = require('./services/logger');
 
 const app = express();
@@ -26,6 +21,16 @@ app.use('/api/auth', authRoutes);
 app.use('/api/items', itemRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/global', globalRoutes);
+
+const options = {
+  customCss: '.swagger-ui .topbar { display: none }',
+};
+
+app.use(
+  '/api/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, options)
+);
 
 const server = app.listen(port, () => {
   logger.log('info', `Server is running on port ${port}`);
