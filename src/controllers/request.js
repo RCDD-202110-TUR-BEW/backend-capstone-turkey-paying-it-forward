57d9cd73dff03fb3ac471f244fccdd735e7b9698
mahmoud <mahmoudalkasem1997@gmail.com>
2022-02-22 17:55:41 +0300

    add request controller functions

---
+++ src/controllers/request.js
@@ -0,0 +1,63 @@
+const { ObjectId } = require('mongoose').Types;
+const requestModel = require('../models/request');
+
+module.exports = {
+  getAllRequests: async (req, res) => {
+    try {
+      const requests = await requestModel.find();
+      if (requests.length <= 0) throw new Error('No requests found');
+      res.json(requests);
+    } catch (err) {
+      res.status(422).json({ message: err.message ?? err });
+    }
+  },
+  createRequest: async (req, res) => {
+    try {
+      await requestModel.create(req.body);
+      res.status(201).json({ message: 'request created successfully.' });
+    } catch (err) {
+      res.status(422).json({ message: err.message });
+    }
+  },
+  getRequestById: async (req, res) => {
+    try {
+      const requestId = req.params.id;
+      if (String(new ObjectId(requestId)) !== requestId.toString())
+        throw new Error('Requested request ID is not valid!');
+      const request = await requestModel.findById(requestId);
+      if (!request)
+        throw new Error('There is no request with the provided ID!');
+      res.json(request);
+    } catch (err) {
+      res.status(422).json({ message: err.message ?? err });
+    }
+  },
+  updateRequest: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const updatedRequest = await requestModel.findByIdAndUpdate(
+        id,
+        { $set: req.body },
+        { new: true }
+      );
+      if (!updatedRequest) {
+        throw new Error('The request with the specified ID was not found.');
+      }
+      res.json(updatedRequest);
+    } catch (err) {
+      res.status(422).json({ message: err.message ?? err });
+    }
+  },
+  deleteRequest: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const request = await requestModel.findByIdAndDelete(id);
+      if (!request) {
+        throw new Error('The request with the specified ID was not found.');
+      }
+      res.status(204).end();
+    } catch (err) {
+      res.status(422).json({ message: err.message ?? err });
+    }
+  },
+};
