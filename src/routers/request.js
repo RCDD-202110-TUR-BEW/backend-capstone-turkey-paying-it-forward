010ce8a6243502f695a868fe2c2863cf4d19b60d
mahmoud <mahmoudalkasem1997@gmail.com>
2022-02-22 17:55:05 +0300

    add request route endpoints

--- src/routers/request.js
+++ src/routers/request.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const userAuthenticationMiddleware = require('../middlewares/user-authentication');
+const requestController = require('../controllers/request');
 
 const router = express.Router();
 
-const requestController = require('../controllers/request');
+// GET, POST routes for /api/request/
+router
+  .get('/', requestController.getAllRequests)
+  .post('/', userAuthenticationMiddleware, requestController.createRequest);
+
+//  GET, PUT, DELETE routes for /api/request/:id
+router
+  .get('/:id', requestController.getRequestById)
+  .use('/:id', userAuthenticationMiddleware)
+  .put('/:id', requestController.updateRequest)
+  .delete('/:id', requestController.deleteRequest);
 
 module.exports = router;
