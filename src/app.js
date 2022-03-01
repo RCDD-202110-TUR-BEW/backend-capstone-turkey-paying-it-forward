af94edd715eb516e32a4ebbe14a47d0b7d1af92d
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-03-02 00:40:50 +0300

    add packages

--- src/app.js
+++ src/app.js
@@ -1,26 +1,46 @@
 const express = require('express');
-
+const path = require('path');
 require('dotenv').config();
-
 const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 const { connectToMongo } = require('./db/connection');
-
 const authRoutes = require('./routers/auth');
-
 const itemRoutes = require('./routers/item');
-
 const userRoutes = require('./routers/user');
-
 const globalRoutes = require('./routers/global');
+const requestRoutes = require('./routers/request');
 
 const logger = require('./services/logger');
+const { googleConfigs, afterGoogleLogin } = require('./passport');
 
 const app = express();
+
 const port = process.env.NODE_LOCAL_PORT;
 
-app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: process.env.JWT_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.static(path.join(__dirname, '/assets')));
 app.use(cookieParser());
+
+passport.use(new GoogleStrategy(googleConfigs, afterGoogleLogin));
+
+app.use(passport.initialize());
 
 app.get('/status', (req, res) => {
   res.send('OK');
@@ -28,7 +48,11 @@ app.get('/status', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/items', itemRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/requests', requestRoutes);
 app.use('/api/global', globalRoutes);
+app.use('/', (req, res) => {
+  res.render('home');
+});
 
 const server = app.listen(port, () => {
   logger.log('info', `Server is running on port ${port}`);
